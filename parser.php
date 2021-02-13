282edd4174fb0e039a6c3f3de18c15387d5bd535
Mher Shahinyan <shahinyanm1@gmail.com>
2021-02-13 11:50:46 +0400

    Initial commit

--- parser.php
+++
@@ -1,8 +0,0 @@
-<?php
-$request = json_decode(file_get_contents('php://input'));
-$response = [];
-if (isset($request->url)){
-    $response = file_get_contents($request->url);
-}
-echo $response;
-
--- resources/js/index.js
+++ resources/js/index.js
@@ -1,32 +1,24 @@
 let data = {};
 let lastComics = 0;
+
 window.onload = function () {
     fetchData();
-    getLastComics();
 };
 
 function fetchData() {
-    let url = 'http://xkcd.com/info.0.json';
+    let url = 'https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json';
     let id = getHrefId();
     if (id != null) {
-        url = `http://xkcd.com/${id}/info.0.json`
+        url = `https://cors-anywhere.herokuapp.com/http://xkcd.com/${id}/info.0.json`
     }
-    fetch('http://comics.test/parser.php', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            url: url
-        })
-    }).then(response => response.json())
+    fetch(url).then(response => response.json())
         .then(response => {
             data = response;
             document.getElementById("header").innerHTML = data.title ?? 'Title is missing';
             document.getElementById("date").innerHTML = data.day + '.' + data.month + '.' + data.year ?? 'Date is missing';
             document.getElementById("main-img").src = data.img ?? 'Image is missing';
             document.getElementById("text").innerHTML = JSON.parse(JSON.stringify(data.transcript)) ?? 'Text is missing';
+            getLastComics();
         });
 }
 
@@ -41,16 +33,7 @@ function getHrefId() {
 }
 
 function getLastComics() {
-    fetch('http://comics.test/parser.php', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            url: 'http://xkcd.com/info.0.json'
-        })
-    }).then(response => response.json())
+    fetch('https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json').then(response => response.json())
         .then(data => {
             lastComics = data.num;
         });
@@ -62,29 +45,31 @@ function actionResolver(value) {
     let urlToReload = 'http://comics.test';
     switch (value) {
         case 'first':
-            urlToReload = '?comicsNumber=1';
+            urlToReload += '?comicsNumber=1';
             break;
         case 'prev':
             if (id > 1) {
-                urlToReload = `?comicsNumber=${id - 1}`;
+                urlToReload += `?comicsNumber=${id - 1}`;
             }
             if (id === null) {
-                urlToReload = `?comicsNumber=${(lastComics - 1)}`;
+                urlToReload += `?comicsNumber=${(lastComics - 1)}`;
             }
             break;
         case 'next':
             if (id !== null) {
-                urlToReload = `?comicsNumber=${Number(id) + 1}`;
+                urlToReload += `?comicsNumber=${Number(id) + 1}`;
+            }else{
+                urlToReload += '?comicsNumber=1';
             }
             break;
         case 'random':
-            urlToReload = `?comicsNumber=${random}`;
+            urlToReload += `?comicsNumber=${random}`;
             break;
         case 'last':
-            urlToReload = '/';
+            urlToReload += '/';
             break;
         default:
-            urlToReload = '/';
+            urlToReload += '/';
     }
     window.location.href = urlToReload;
 }
